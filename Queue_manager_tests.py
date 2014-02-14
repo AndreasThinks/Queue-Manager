53685bd01aac8c1dd4d1daf88c9ddd9bf72528f6
Victor Miclovich <vicmiclovich@gmail.com>
2014-02-14 10:16:59 -0500

    modifications

--- Queue_manager_tests.py
+++ Queue_manager_tests.py
@@ -1,19 +1,25 @@
 __author__ = 'Andreas'
-import unittest, time
-from Queue_manager import *
+import unittest
+import time
+from Queue_manager import Task, Queue
+
 
 class QueueManagerTests(unittest.TestCase):
     def test_create_task(self):
-        passport = Task(20,"passport","Bob")
-        self.assertTrue(isinstance(passport, Task))
+        passport = Task(20, "passport", "Bob")
+        self.assertTrue(isinstance(passport, Task))  # could this test be more elaborate?
+        # TODO -->
+        # For example
+        # self.assertEquals(passport.time, 20)
+        # self.assertEquals(task_type, "passport")
 
     def test_create_queue(self):
         new_queue = Queue()
-        self.assertTrue(isinstance(new_queue.startTime, float) )
+        self.assertTrue(isinstance(new_queue.startTime, float))
 
     def test_add_to_queue(self):
         new_queue = Queue()
-        passport = Task(20,"passport","Bob")
+        passport = Task(20, "passport", "Bob")
         new_queue.add_to_queue(passport)
         self.assertTrue(passport in new_queue.tasks)
 
